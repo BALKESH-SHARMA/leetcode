e950732b98cb72f1e11a074a718419cf6cd8e011
BALKESH <74245824+BALKESH-SHARMA@users.noreply.github.com>
2024-02-03 13:47:56 +0530

    Time: 15 ms (82.46%), Space: 26.5 MB (18.10%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -28,4 +28,19 @@ public:
         }
         return nums;
     }
+            //other way
+//      int prefix = 1;
+//         int postfix = 1;
+//         int n = nums.size();
+//         vector<int> result(n,1);
+//         for(int i=0;i<n;i++){
+//             result[i] = prefix;
+//             prefix = prefix * nums[i];
+//         }
+//         for(int i=n-1;i>=0;i--){
+//             result[i] = result[i] * postfix;
+//             postfix = postfix * nums[i];
+//         }
+        
+//         return result;
 };
