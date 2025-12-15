b9fd3d5c6b7945fcc1d0a7f288e7d21095c1de5e
BALKESH <74245824+BALKESH-SHARMA@users.noreply.github.com>
2025-12-15 13:53:26 +0530

    Time: 0 ms (100%), Space: 21.9 MB (73.79%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_set<char>st;
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(board[i][j]=='.')continue;
-                if(st.find(board[i][j])!= st.end())return false;
-                st.insert(board[i][j]);
+        bool row[9][9] = {false};
+        bool col[9][9] = {false};
+        bool box[9][9] = {false};
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                int num = board[i][j] - '1';          
+                int k = (i / 3) * 3 + (j / 3);
+    
+                if (row[i][num] || col[j][num] || box[k][num])
+                    return false;
+    
+                row[i][num] = col[j][num] = box[k][num] = true;
             }
-            st.clear();
-        }
-        for(int j=0;j<9;j++){
-            for(int i=0;i<9;i++){
-                if(board[i][j]=='.')continue;
-                if(st.find(board[i][j])!= st.end())return false;
-                st.insert(board[i][j]);
-            }
-            st.clear();
-        }
-        for (int boxRow = 0; boxRow < 3; boxRow++) {
-          for (int boxCol = 0; boxCol < 3; boxCol++) {
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    char c = board[boxRow*3 + i][boxCol*3 + j];
-                    if (c == '.') continue;
-                    if (st.find(c) != st.end()) return false;
-                    st.insert(c);
-                }
-            }
-            st.clear();
-          }
         }
         return true;
     }
